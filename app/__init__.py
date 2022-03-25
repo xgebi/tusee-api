ba99e90217fd02356410f237b1ea10919dd44ae4
Sarah Gebauer <xgebi@users.noreply.github.com>
2022-03-25 22:37:26 +0100

    migration stuff 😏

--- app/__init__.py
+++ app/__init__.py
@@ -8,10 +8,10 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile(os.path.join(os.getcwd(), 'config', f'{os.environ["FLASK_ENV"]}.py'))
+
     cors = CORS(app)
     db.init_app(app)
-
-    app.config.from_pyfile(os.path.join(os.getcwd(), 'config', f'{os.environ["FLASK_ENV"]}.py'))
 
     from app.authentication import authentication
     app.register_blueprint(authentication)
