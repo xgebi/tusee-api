d3287ff643f7f6c1bba7b1df3b372e6300c16295
Sarah Gebauer <xgebi@users.noreply.github.com>
2022-09-17 11:33:00 +0200

    Note returns dates as timestamps 🕐

--- app/utils/note_tasks.py
+++ app/utils/note_tasks.py
@@ -9,23 +9,26 @@ import psycopg
 def get_notes_for_user(*args, user_uuid: str, connection: psycopg.Connection, **kwargs):
 	with connection.cursor(row_factory=psycopg.rows.dict_row) as cur:
 		cur.execute(
-			"""SELECT note_uuid, user_uuid, title, updated
+			"""SELECT note_uuid, user_uuid, title,
+			(EXTRACT (EPOCH FROM updated) * 1000)::bigint AS updated
 			FROM tusee_notes WHERE user_uuid = %s ORDER BY updated DESC;""",
 			(user_uuid, )
 		)
-		temps = cur.fetchall()
-		return temps
+		notes = cur.fetchall()
+		return notes
 
 
 def get_note_task(*args, user_uuid: str, connection: psycopg.Connection, note_uuid: str, **kwargs):
 	with connection.cursor(row_factory=psycopg.rows.dict_row) as cur:
 		cur.execute(
-			"""SELECT note_uuid, user_uuid, title, note, created, updated
+			"""SELECT note_uuid, user_uuid, title, note,
+			(EXTRACT (EPOCH FROM created) * 1000)::bigint AS created,
+			(EXTRACT (EPOCH FROM updated) * 1000)::bigint AS updated
 			FROM tusee_notes WHERE user_uuid = %s AND note_uuid = %s;""",
 			(user_uuid, note_uuid)
 		)
-		temps = cur.fetchone()
-		return temps
+		temp = cur.fetchone()
+		return temp
 
 
 def create_note_task(*args, user_uuid: str, connection: psycopg.Connection, note: Dict, **kwargs):
@@ -45,7 +48,8 @@ def update_note_task(*args, user_uuid: str, connection: psycopg.Connection, note
 	with connection.cursor(row_factory=psycopg.rows.dict_row) as cur:
 		cur.execute(
 			"""UPDATE tusee_notes SET title = %s, note = %s, updated = %s WHERE user_uuid = %s AND note_uuid = %s
-			RETURNING user_uuid, note_uuid, title, note, created, updated""",
+			RETURNING user_uuid, note_uuid, title, note, (EXTRACT (EPOCH FROM created) * 1000)::bigint AS created,
+			(EXTRACT (EPOCH FROM updated) * 1000)::bigint AS updated""",
 			(note['title'], note['note'], datetime.now(), user_uuid, note['note_uuid'])
 		)
 		temps = cur.fetchone()
